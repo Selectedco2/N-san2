15dd9c1da7394165c4daf62e08ba4f8ab29220fd
Andreas Thegelström <andreas.thegelstrom@elev.ga.ntig.sem>
2024-11-29 13:24:12 +0100

    Klar med alla

---
+++ The Nose2/Program.cs
@@ -0,0 +1,62 @@
+﻿// using System.Globalization;
+// using System.Linq.Expressions;
+
+// List<string> leksaker = ["Lego", "ScoobyDoo", "Transformer", "Bakugan", "Beyblades"];
+
+// // ----------------------------------------------------------------------------------------
+
+// foreach (string leksak in leksaker)
+// {
+//     Console.WriteLine(leksak);
+// }
+
+// // ----------------------------------------------------------------------------------------
+
+// List<string> personer = ["Vincent", "Skoogen", "Kacper", "Yosuf", "Noah"];
+
+// // ----------------------------------------------------------------------------------------
+
+// List<int> siffror = [1, 2, 3, 4, 5];
+
+// // ----------------------------------------------------------------------------------------
+
+// for (int i = 0; i < leksaker.Count; i++)
+// {
+//     Console.WriteLine(personer[i] + " ger " + leksaker[i] + " betyget " + siffror[i]);
+// }
+
+// Console.ReadKey();
+// // ----------------------------------------------------------------------------------------
+
+// // List<string> cities = [];
+
+// // // ----------------------------------------------------------------------------------------
+// // string city = "";
+
+// // Console.WriteLine("Vilken stad vill du lägga till? Skriv exit för att stoppa.");
+
+// // while (city != "exit")
+// // {
+// //     city = Console.ReadLine().ToLower();
+
+// //     if (city != "exit")
+// //     {
+// //         cities.Add(city);
+// //         Console.WriteLine("Någon mer?");
+// //     }
+
+// // }
+
+// //     Console.WriteLine("Här är alla dina städer:");
+
+// // foreach (string stad in cities)
+// // {
+// //     Console.WriteLine(stad);
+// // }
+
+// // Console.ReadKey();
+
+
+
+
+
